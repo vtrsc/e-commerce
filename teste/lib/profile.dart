7f2a5f90cc5111497a9c133284b0556773fd9ad7
Vitor Salazar <vitorsc1998@gmail.com>
2022-08-02 12:52:17 -0300

    upload style text bold

--- teste/lib/profile.dart
+++ teste/lib/profile.dart
@@ -18,7 +18,8 @@ class profile extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
                   gradient: LinearGradient(
-                colors: <Color>[  Color.fromARGB(255, 15, 23, 173).withOpacity(0.8),
+                colors: <Color>[ 
+                   Color.fromARGB(255, 15, 23, 173).withOpacity(0.8),
             Color.fromARGB(255, 15, 133, 232).withOpacity(0.9)],
               )),
             ),
@@ -74,7 +75,8 @@ class profile extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-              Text("I believe that technology can help us with today's problems, look to the future and improve the quality of life with products and services.\n Ability to manage IT areas and process services, focusing on application development productivity and business support.\n  my mission is to offer tailor-made infrastructure support services, based on industry-proven technologies and solutions.New technologies and advanced solution options with speed and flexibility. \n I have the expertise to provide IT services that are personal and offer fast turnaround. I will support you in your business growth by working with you to implement solutions with the goal to improve your own productivity and increase your productivity with external partners. job as a mobile app developer is to make everyday tasks easier. Without a doubt, this is a career that will only get bigger and more important in the future.", 
+              Text("I believe that technology can help us with today's problems, look to the future and improve the quality of life with products and services.\n Ability to manage IT areas and process services, focusing on application development productivity and business support.\n  my mission is to offer tailor-made infrastructure support services, based on industry-proven technologies and solutions.New technologies and advanced solution options with speed and flexibility. \n I have the expertise to provide IT services that are personal and offer fast turnaround. I will support you in your business growth by working with you to implement solutions with the goal to improve your own productivity and increase your productivity with external partners. job as a mobile app developer is to make everyday tasks easier. Without a doubt, this is a career that will only get bigger and more important in the future.",
+       style: TextStyle( fontWeight: FontWeight.bold,),
               textAlign: TextAlign.start,),
             ],
           ),
